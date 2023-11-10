12b6b69ceadd7f5363b1de0b9e1400a8fe00c994
MA3134346 <maximilian.angres@student.griffith.ie>
2023-11-10 16:35:04 +0000

    Added Sensor functionality Added Compass Rotation Added Current Rotation as Degree

--- app/src/main/java/com/example/mdproject/ui/theme/Compass.kt
+++ app/src/main/java/com/example/mdproject/ui/theme/Compass.kt
@@ -1,13 +1,27 @@
 package com.example.mdproject.ui.theme
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mdproject.R
@@ -15,21 +29,89 @@ import com.example.mdproject.R
 @Preview(showBackground = true)
 @Composable
 fun Compass() {
-    Box(modifier = Modifier.fillMaxWidth(), Alignment.Center){
-        DegreeText()
-    }
-    Box(modifier = Modifier.fillMaxSize(), Alignment.Center){
-        CompassSprite()
+    val rot = rotation()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.5f),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            CompassTarget("North")
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.2f),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            DegreeText(rot)
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(2f),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            CompassSprite(rot)
+        }
     }
 }
+
 @Composable
-fun DegreeText(){
-    Text("Filled")
+fun CompassTarget(target: String){
+    Text(text = "Target: $target")
 }
 @Composable
-fun CompassSprite() {
+fun DegreeText(rotation: Float) {
+    Text(text = "${rotation.toInt()}°")
+}
+
+@Composable
+fun CompassSprite(rotation: Float) {
     Image(
         painter = painterResource(R.drawable.compass),
         contentDescription = "Compass",
+        modifier = Modifier.graphicsLayer {
+            rotationZ = rotation
+        }
     )
+}
+
+@Composable
+fun rotation(): Float {
+    var rot by remember { mutableFloatStateOf(0f) }
+
+    val sensorManager: SensorManager = LocalContext.current.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+    DisposableEffect(Unit) {
+        val listener = object : SensorEventListener {
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+            override fun onSensorChanged(event: SensorEvent) {
+                val rotationMatrix = FloatArray(9)
+                SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values)
+
+                val orientationValues = FloatArray(3)
+                SensorManager.getOrientation(rotationMatrix, orientationValues)
+
+                val azimuth = Math.toDegrees(orientationValues[0].toDouble()).toFloat()
+
+                rot = if (azimuth < 0) azimuth + 360 else azimuth
+            }
+        }
+        sensorManager.registerListener(
+            listener,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
+            SensorManager.SENSOR_DELAY_NORMAL)
+        onDispose {
+            sensorManager.unregisterListener(listener)
+        }
+    }
+    return rot
 }
